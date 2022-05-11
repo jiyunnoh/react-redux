4e86ddf882ffd22a02eccf497166e0489a43d105
jiyunnoh <ianjy1127@gmail.com>
2022-05-11 01:23:43 -0500

    246. Reading & Dispatching From A New Slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
+export type AuthReducerType = {
+  isAuthenticated: boolean
+}
 
 function App() {
+  const isAuth = useSelector((state: { auth: AuthReducerType }) => state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,10 +1,21 @@
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store';
 import classes from './Auth.module.css';
 
 const Auth = () => {
+  const dispath = useDispatch();
+
+  const loginHandler = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+    dispath(authActions.login());
+  }
+
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
